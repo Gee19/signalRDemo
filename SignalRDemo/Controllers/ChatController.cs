96f5e207b112c12077d7b600c807a0e0b935929e
gee19 <gee19haine@gmail.com>
2015-01-31 01:54:12 -0500

    no unique guid implemented, origin

---
+++ SignalRDemo/Controllers/ChatController.cs
@@ -0,0 +1,21 @@
+﻿using SignalRDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalRDemo.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Index()
+        {
+            /*ChatViewModel cvm = new ChatViewModel();
+            cvm.clientID = cvm.guid(5, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+            return View(cvm);*/
+            return View();
+        }
+    }
+}
